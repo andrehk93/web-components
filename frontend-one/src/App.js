59e7815167215c72898bcaa939325202a182181f
Andreas Kvistad <ahk9339@gmail.com>
2021-11-17 21:08:39 +0100

    feat: Add revealjs presentation

--- frontend-one/src/App.js
+++ frontend-one/src/App.js
@@ -6,6 +6,10 @@ function App() {
         <react-header></react-header>
         <main>
             <h2>Application One</h2>
+            <article>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam gravida nulla quis massa euismod, vel pellentesque eros viverra. Etiam vitae euismod enim. Fusce tristique, lacus et ultricies posuere, tortor ipsum malesuada tortor, nec congue lacus libero nec arcu.</p>
+                <p>Nunc eget placerat felis, sit amet pretium dolor. Morbi at diam a magna lacinia egestas eu quis turpis. In hac habitasse platea dictumst. Nulla eu enim lectus. Duis quis nulla auctor, ornare nunc id, commodo dui. Integer at orci volutpat, rhoncus lacus et, auctor velit. Maecenas id magna sed est rutrum porttitor vitae egestas velit. Donec dictum, sem vel egestas vehicula, est dui pharetra lectus, sed sollicitudin risus metus a risus.</p>
+            </article>
         </main>
         <vue-footer></vue-footer>
     </div>
